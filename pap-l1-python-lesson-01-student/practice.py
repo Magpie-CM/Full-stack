5fc158faba891b056de8705a232d4796c468c3f4
Magpie <anastasia.meneely@gmail.com>
2025-05-14 22:00:47 -0700

    add lesson 1 code

---
+++ pap-l1-python-lesson-01-student/practice.py
@@ -0,0 +1,23 @@
+# Exercise 1: Make a new comment
+#hi
+
+
+# Exercise 2: Create 5 variables
+# Hint: you can try values that are just numbers too, 
+# but you don't need to put quotes around them, for example: var = 10
+var1=1
+var2=2
+var3=3
+var4=4
+var5=5
+# Exercise 3: print all 5 variables
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+
+# Exercise 4: Try commenting out some of your code, what happens?
+#this function lists varible values and then prints them
+# Exercise 5: Make sure all of your code works, revert changes if necessary.
+# Turn in the url of this page on Canvas.
